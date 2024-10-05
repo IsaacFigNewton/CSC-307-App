a94376ff2e67b822e69687fac900a38b4326eac4
Isaac <48895941+IsaacFigNewton@users.noreply.github.com>
2024-10-05 14:21:41 -0700

    Added a form
    To create new entries in the table

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,10 @@
 // src/MyApp.jsx
 import React, { useState} from "react";
 import Table from "./Table";
+import Form from "./Form"
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -30,12 +14,18 @@ function MyApp() {
   }
 
   return (
-    <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-    </div>
+    // <div>
+      // <h1>My First React Web App!</h1>
+      <div className="container">
+        {/* <h2>People and their Jobs</h2> */}
+        <Table
+          characterData={characters}
+          removeCharacter={removeOneCharacter}
+        />
+        {/* <h2>Add someone new</h2> */}
+        <Form />
+      </div>
+    // </div>
   );
 }
 
