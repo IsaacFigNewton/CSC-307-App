e7e78ba6fcbdf7d7d8ade6a05b4d0c3adcdb6d73
Isaac <48895941+IsaacFigNewton@users.noreply.github.com>
2024-10-12 14:30:26 -0700

    Finished adding entry deletion with DELETE request and more granular filtering request handling.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,29 +45,46 @@ const addUser = (user) => {
   users["users_list"].push(user);
   return user;
 };
-
+const filterUsersByNameJob = (name, job) => {
+  return users["users_list"].filter(
+      (user) => (user["name"] === name) && (user["job"] === job)
+  );
+};
+const removeUserById = (id) => {
+  users["users_list"] = users["users_list"].filter(
+    (user) => user["id"] != id
+  );
+  return id;
+};
 
 // instantiate the app
 app.use(express.json());
 
 // get index page
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// get users page
-app.get("/users", (req, res) => {
-    res.send(JSON.stringify(users) + " no one expects the JS inquisition!!");
+  res.send("<h1>Hello World!</h1><br>\
+    <h2>Go to <a href=\"http://localhost:8000/users\">this link</a>\
+    to see a list of all users.</h2><br>\
+    <h2>Or <a href=\"http://localhost:8000/users?name=Mac&job=Bouncer\">this link</a>\
+    to filter to just Mac from It's Always Sunny in Philadelphia.</h2>");
 });
 
 // get user with query
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+  if (name != undefined && job != undefined) {
+    // get all users that match a given name and a given job.
+      let result = filterUsersByNameJob(name, job);
+      result = { users_list: result };
+      res.send(result);
+  } else if (name != undefined) {
+    // get all users that match a given name.
       let result = findUserByName(name);
       result = { users_list: result };
       res.send(result);
   } else {
+    // if no query provided, just send the JSON of all the users
       res.send(users);
   }
 });
@@ -83,16 +100,27 @@ if (result === undefined) {
 }
 });
 
-// listen for requests
-app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
-});
-
 // add a user based on the request body if it's a POST request
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+// remove a particular user by id from the users list
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = removeUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+  });
+  
+// listen for requests
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
 });
